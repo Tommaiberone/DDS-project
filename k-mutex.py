bba8e686f4f18da5a8ffff263c9c93725a2d6118
Tommaiberone <bersani.1840825@studenti.uniroma1.it>
2023-03-13 12:11:57 +0100

    Removed randomness and added timestamps

--- k-mutex.py
+++ k-mutex.py
@@ -1,17 +1,16 @@
 import threading
-#import tcp
-import queue
 import pymq
 from pymq import EventBus
 from pymq.provider.redis import RedisConfig
-import random
 import time
 
-# Constants
+# ConstantimedServer
 K = 5
 N = 10
 BROADCAST = 1899
+TIME = 0
 threads = [0]*N
+CHATTY = False
 
 # Define a message class
 class Msg:
@@ -29,37 +28,58 @@ class TimeServer:
 	
 	# Handler for incoming messages
 	def handle_message(self, message : Msg):
+
 		return
 	
 	# Constructor
 	def __init__(self, bus: EventBus):
+
 		self.bus = bus
 		self.bus.subscribe(self.handle_message)
 
-	# Starts worker threads
+	# StartimedServer worker threads
 	def start_threads(self, bus):
+
 		for i in range(N):
 			threads[i] = threading.Thread(target=thread_function, args=(i, bus))
 			threads[i].start()
+
+		for i in range(N):
+			threads[i].join()
 	
 	# Periodically sends GO messages to random processes
 	def timed_countdown(self):
-		while True:
-			timeout = random.randint(0, 10)
-			random_process = random.randint(0, N-1)
-			time.sleep(timeout)
 
-			# Send a GO message to a random process
-			msg = Msg()
-			msg.kind = "GO"
-			msg.mit = 0
-			msg.dest = random_process
-			msg.seq = 0
-			msg.num_rep = 0
-			self.bus.publish(msg)
+		with open('values.txt', 'r') as file:
+			values = file.read()
+			values = values.replace("\n", "").replace("  ", " ").split(" ")
+			float_values = list(map(float, values))
+
+			with open('processes.txt', 'r') as file2:
+				processes = file2.read()
+				processes = processes.replace("\n", "").split(" ")
+				int_processes = list(map(int, processes))
+
+
+				for i in range(len(float_values)):
+
+					timeout = float_values[i]
+					random_process = int_processes[i]
+					
+					time.sleep(timeout)
+
+					# Send a GO message to a random process
+					msg = Msg()
+					msg.kind = "GO"
+					msg.mit = 0
+					msg.dest = random_process
+					msg.seq = 0
+					msg.num_rep = 0
+					self.bus.publish(msg)
 
 # Define a Thread class
 class Thread:
+
 	pid : int
 	req_cs : bool
 	cs:	bool
@@ -67,9 +87,11 @@ class Thread:
 	maxseq : int
 	def_c: [0]*N
 	reply_count: [0]*N
+	time : float
 	
 	# Constructor
 	def __init__(self,bus: EventBus,pid: int):
+
 		self.pid = pid
 		self.req_cs = False
 		self.cs = False
@@ -78,7 +100,8 @@ class Thread:
 		self.bus = bus
 		self.def_c= [0]*N
 		self.reply_count= [0]*N
-		self.bus.subscribe(self.handle_message)	
+		self.bus.subscribe(self.handle_message)
+		time = 0	
 		
 	# Handler for incoming messages
 	def handle_message(self, message : Msg):
@@ -107,18 +130,24 @@ class Thread:
 
 				      # Check if the message is a reply to a request for the critical section
 				elif message.kind == "REPLY":
+
 					# Update the reply_count variable
-					self.reply_count[message.mit] += 1
+					self.reply_count[message.mit] -= message.num_rep
 					
-					# Check if the process has received replies from all processes except itself and can enter the critical section
+					# Check if the process has received replies from all processes except itimedServerelf and can enter the critical section
 					if self.req_cs and (self.not_in_cs() >= N-K):
+
 						self.req_cs = False
-						print("sono" + str(self.pid) + "e entro in cs\n")
+						if CHATTY: print("sono" + str(self.pid) + "e entro in cs\n")
+						
+						print("Sono entrato in cs dopo:", time.time() - self.time, "secondi")
+						
 						self.cs = True
+
 						self.do_cs_stuff()
 						self.cs = False
 						
-						# Send replies to deferred requests
+						# Send replies to deferred requestimedServer
 						for i in range(0, N):
 							if self.def_c[i] != 0:
 								self.send("REPLY", self.pid, i, self.maxseq, self.def_c[i])
@@ -126,19 +155,25 @@ class Thread:
 								
 				# Check if the message is a signal to request access to the critical section
 				elif message.kind == "GO":
-					# Update the request variables
-					self.req_cs = True
-					self.seq = self.maxseq + 1
-					print("sono"+str(self.pid)+"e mando una req\n")
-					# Send a request message to all processes and update the reply_count variable
-					self.send("REQ", self.pid, BROADCAST, self.seq, 0)
-					
-					for i in range(0, N):
-						self.reply_count[i] += 1
+
+					if self.req_cs == False:
+
+						self.time = time.time()
+						
+						# Update the request variables
+						self.req_cs = True
+						self.seq = self.maxseq + 1
+						if CHATTY: print("sono"+str(self.pid)+"e mando una req\n")
+						# Send a request message to all processes and update the reply_count variable
+						self.send("REQ", self.pid, BROADCAST, self.seq, 0)
+						
+						for i in range(0, N):
+							self.reply_count[i] += 1
 
 
 			
 	def send(self, kind: str, mit: int, dest: int, seq: int, num: int):
+
 		# Define a function for sending a message to the message bus
 		# and update the message attributes
 		m = Msg()
@@ -150,38 +185,40 @@ class Thread:
 		self.bus.publish(m)
 
 	def not_in_cs(self):
+
 		# Define a function for counting the number of processes that have not replied
 		# to a request message sent by this process
-		print("sono "+str(self.pid)+" e conto le reply")
+		if CHATTY: print("sono "+str(self.pid)+" e conto le reply")
 		c = 0
+
 		for i in range(0, N):
-			print(self.reply_count[i])
+			#print(self.reply_count[i])
 			if i != self.pid and self.reply_count[i] == 0:
 				c += 1
+
 		return c
 		
 	def do_cs_stuff(self):
+
 		# Define a function that simulates the process performing some critical section work
 		print("inizio a lavora")
-		time.sleep(random.randint(0,5))
 		print("vaffanculo vado a casa")
 
 def thread_function(pid, bus):
+
 	# Define a function that runs a thread for a specific process ID and message bus
 	thread = Thread(bus, pid)
-	thread.run()
 
 def main():
+
 	# Initialize the message bus and the time server
 	bus = pymq.init(RedisConfig())
-	ts = TimeServer(bus)
+	timedServer = TimeServer(bus)
 	
-	# Start the threads and the time server
-	for i in range(0, N):
-		t = Thread(target=thread_function, args=(i, bus))
-		t.start()
-	ts.start_threads(bus)
-	ts.timed_countdown()
+	timedServer.start_threads(bus)
+	timedServer.timed_countdown()
+
+	TIME = time.time()
 
 if __name__ == '__main__':
     main()
---
+++ temp.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.random.randint(low=0, high=9, size=200)
+
+print(arr)
+
+#with open('values.txt', 'r') as file:
+#			values = file.read()
+#			values = values[1::].replace("\n", "").replace("]", "").split(" ")
+#			print(values[-1])
